999c7509e60402a56638e4bf8939d6cfd2540028
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2022-03-20 17:47:47 -0500

    Add hydra

--- public/script.js
+++ public/script.js
@@ -3,7 +3,9 @@ const liveView = document.getElementById("liveView");
 const demosSection = document.getElementById("demos");
 const enableWebcamButton = document.getElementById("webcamButton");
 const canvas = document.getElementById("output");
+const hydra_canvas = document.getElementById("hydra");
 const osc = new OSC();
+var hydra = new Hydra({ canvas: hydra_canvas });
 osc.open();
 var ctx = undefined;
 // TODO
@@ -11,70 +13,98 @@ var detector = undefined;
 const score_thresh = 0.3;
 
 tf.loadGraphModel("public/movenet-thunder/model.json").then((loadedModel) => {
-    const detectorConfig = {
-        modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER,
-    };
-    poseDetection
-        .createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig)
-        .then((detectorObject) => {
-            detector = detectorObject;
-            if (canvas.getContext) {
-                ctx = canvas.getContext("2d");
-            }
-            demosSection.classList.remove("invisible");
-        });
+  const detectorConfig = {
+    modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER,
+  };
+  poseDetection
+    .createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig)
+    .then((detectorObject) => {
+      detector = detectorObject;
+      if (canvas.getContext) {
+        ctx = canvas.getContext("2d");
+      }
+      demosSection.classList.remove("invisible");
+    });
 });
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
-    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
 // If webcam supported, add event listener to button for when user
 // wants to activate it to call enableCam function which we will
 // define in the next step.
 if (getUserMediaSupported()) {
-    enableWebcamButton.addEventListener("click", enableCam);
+  enableWebcamButton.addEventListener("click", enableCam);
 } else {
-    console.warn("getUserMedia() is not supported by your browser");
+  console.warn("getUserMedia() is not supported by your browser");
 }
 
 // Enable the live webcam view and start classification.
 function enableCam(event) {
-    // Only continue if the COCO-SSD has finished loading.
-    if (!detector) {
-        return;
-    }
+  // Only continue if the COCO-SSD has finished loading.
+  if (!detector) {
+    return;
+  }
 
-    // Hide the button once clicked.
-    event.target.classList.add("removed");
+  // Hide the button once clicked.
+  event.target.classList.add("removed");
 
-    // getUsermedia parameters to force video but not audio.
-    const constraints = {
-        video: true,
-    };
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true,
+  };
 
-    video.onloadedmetadata = () => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-    };
-
-    // Activate the webcam stream.
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        video.srcObject = stream;
-        video.addEventListener("loadeddata", predictWebcam);
+  video.onloadedmetadata = () => {
+    vw = video.videoWidth / 2;
+    vh = video.videoHeight / 2;
+    video.width = vw;
+    video.height = vh;
+    canvas.width = vw;
+    canvas.height = vh;
+    hydra_canvas.width = vw;
+    hydra_canvas.height = vh;
+    hydra = new Hydra({
+      canvas: hydra_canvas,
+      height: vh,
+      width: vw,
     });
+    hydra.synth.osc(4, 0.1, 1.2).out();
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadeddata", predictWebcam);
+  });
+}
+
+function drawHydra(pose) {
+  hydra.synth
+    .osc(5, 0.1)
+    .modulate(noise(8), 0.22)
+    .diff(o0)
+    .modulateScrollY(
+      hydra.synth.osc(2).modulate(hydra.synth.osc().rotate(), 0.11)
+    )
+    .scale(0.72)
+    .color(0.99, 1.014, 1)
+    .scale(pose.nose["x"])
+    .out(o0);
 }
 
 // Placeholder function for next step.
 function predictWebcam() {
-    detector.estimatePoses(video).then((preds) => {
-        const kp = preds[0]["keypoints"];
-        const normal_kp = poseDetection.calculators.keypointsToNormalizedKeypoints(
-            kp, { width: video.videoWidth, height: video.videoHeight }
-        );
-        pose = new Pose(normal_kp);
-        pose.draw(score_thresh, ctx);
-    });
-    window.requestAnimationFrame(predictWebcam);
+  detector.estimatePoses(video).then((preds) => {
+    const kp = preds[0]["keypoints"];
+    const normal_kp = poseDetection.calculators.keypointsToNormalizedKeypoints(
+      kp,
+      { width: video.videoWidth, height: video.videoHeight }
+    );
+    pose = new Pose(normal_kp);
+    pose.draw(score_thresh, ctx);
+    drawHydra(pose);
+  });
+  window.requestAnimationFrame(predictWebcam);
 }
--- public_requires.js
+++ public_requires.js
@@ -1,3 +1,5 @@
 const OSC = require("osc-js");
+const Hydra = require("hydra-synth");
 
 global.window.OSC = OSC;
+global.window.Hydra = Hydra;
